d0727ba8acea16f9bbb3c738b62510262649e675
Igo B. Monteiro <igobmonteiro@gmail.com>
2024-03-25 17:17:02 -0300

    remove console log

--- controllers/CreateRoutes.ts
+++ controllers/CreateRoutes.ts
@@ -2,7 +2,6 @@ import { checkHealth } from '../health';
 
 export const CreateRoutes = app => {
   app.get('/health', async (_, response) => {
-    console.log('health po');
     const health = await checkHealth();
     if (!health.ok) {
       return response.status(500).json(health);
